73779f4025d7f32ae37bfe80f07f8e83c601c136
alvarobrm <alvaroberrocal@MacBook-Air-de-Alvaro.local>
2015-03-11 14:24:45 +0100

    aaa

--- src/java/Sevlets/ServletAplanador.java
+++ src/java/Sevlets/ServletAplanador.java
@@ -13,8 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.thoughtworks.xstream.XStream;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
