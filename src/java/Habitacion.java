25312c60136d568d522e4fc9bcd493508742f903
alvarobrm <alvaroberrocal@MacBook-Air-de-Alvaro.local>
2015-03-11 13:55:16 +0100

    Clase Habitación

--- src/java/Habitacion.java
+++ src/java/Habitacion.java
@@ -10,5 +10,31 @@
  */
 public class Habitacion {
     private int numero;
+    private Reserva reserva;
+
+    public Habitacion(int numero, Reserva reserva) {
+        this.numero=numero;
+        this.reserva=reserva;
+    }
     
+    public boolean isReserved(){
+        return(reserva==null);
+    }
+    
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Reserva getReserva() {
+        return reserva;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    } 
 }
